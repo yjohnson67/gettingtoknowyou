238b8d188aa081c72541312b8e3c9307e952667e
yjohnson67 <joh23004@byui.edu>
2026-02-08 17:55:20 -0600

    form submission

--- greeting_project/greeting/views.py
+++ greeting_project/greeting/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        color = request.POST.get('color')
+ 
+        if not name:
+            return render(request, 'home.html', {'error':'Name required'})
+ 
+        return render(request, 'result.html', {'name':name, 'color':color})
+ 
     return render(request, 'home.html')
 
