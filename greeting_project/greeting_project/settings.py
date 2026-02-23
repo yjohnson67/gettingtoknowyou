df8d43c1cc351c7d7568ecb53e53f86634ab9d47
yjohnson67 <joh23004@byui.edu>
2026-02-22 19:46:45 -0600

    Adding code to settings

--- greeting_project/greeting_project/settings.py
+++ greeting_project/greeting_project/settings.py
@@ -117,4 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+X_FRAME_OPTIONS = 'ALLOWALL'
