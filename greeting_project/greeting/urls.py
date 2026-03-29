774f492ee050aeb72f1b568836af53e2ebab790e
EChlarson <chl20001@byui.edu>
2026-03-28 21:22:24 -0500

    connection to db fixed

--- greeting_project/greeting/urls.py
+++ greeting_project/greeting/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('save-test-score/', views.save_test_score, name='save_test_score'),
 ]
--- greeting_project/greeting_project/settings.py
+++ greeting_project/greeting_project/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import os
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +118,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 X_FRAME_OPTIONS = 'ALLOWALL'
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_DB_NAME = "rocket_game"
